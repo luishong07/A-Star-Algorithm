0023a7bbb289685b2daa71933e8712d5c8953380
Luis Hong <luishong50@gmail.com>
2023-04-20 17:09:30 -0500

    the algorithm advances; not sure if in the right manner; but previos bug was an indexing problem in the Spot class

--- sketch.js
+++ sketch.js
@@ -8,12 +8,12 @@ let grid = new Array(cols);
 let start;
 let end;
 
-let w
-let h
+let w;
+let h;
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    w = width/ cols
-    h = height / rows
+    w = width / cols;
+    h = height / rows;
     //2d array
     for (let i = 0; i < cols; i++) {
         grid[i] = new Array(rows);
@@ -21,36 +21,94 @@ function setup() {
 
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
-            grid[i][j] = new Spot(i,j);
+            grid[i][j] = new Spot(i, j);
+        }
+    }
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j].addNeighbors(grid);
         }
     }
 
     start = grid[0][0];
     end = grid[cols - 1][rows - 1];
 
-    openSet.push(start)
+    openSet.push(start);
 }
 
-function draw() {
+function removeFromArray(arr, element) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if(arr[i]== element){
+            arr.splice(i, 1); 
+        }
+    }
+}
 
-    if(openSet.length>0){
+function heuristic(a,b){
+    let d = dist(a.i,a.j,b.i,b.j)
+    return d
+}
+
+
+function draw() {
+    if (openSet.length > 0) {
         //we keep going
-    }else{
+        let winner = 0;
+        for (let i = 0; i < openSet.length; i++) {
+            if (openSet[i].f < openSet[winner]) {
+                winner = i;
+            }
+        }
+
+        let current = openSet[winner];
+
+        if (current == end) {
+            console.log("Done");
+        }
+        removeFromArray(openSet, current);
+        closedSet.push(current);
+
+        let neighbors = current.neighbors;
+        for (let i = 0; i < neighbors.length; i++) {
+            let neighbor = neighbors[i];
+
+            if (!closedSet.includes(neighbor)) {
+                let tempG = current.g + 1;
+
+                if (openSet.includes(neighbor)) {
+                    if(tempG < neighbor.g){
+                        neighbor.g = tempG
+                    }
+                }else{
+                    neighbor.g = tempG
+                    openSet.push(neighbor)
+                }
+
+                neighbor.h = heuristic(neighbor, end)
+                neighbor.f = neighbor.g + neighbor.h
+
+                
+            }
+        }
+    } else {
         //no solution
     }
     background(0);
 
-    for(let  i = 0; i< cols;i++){
-        for(let j = 0; j < rows; j++){
-            grid[i][j].show(color(255))
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j].show(color(255));
         }
     }
 
-    for(let i = 0; i < closedSet.length; i++){
-        closedSet[i].show(color(255,0,0))
+    for (let i = 0; i < closedSet.length; i++) {
+        closedSet[i].show(color(255, 0, 0));
     }
 
-    for(let i = 0; i < openSet.length; i++){
-        openSet[i].show(color(0,255,0))
+    for (let i = 0; i < openSet.length; i++) {
+        openSet[i].show(color(0, 255, 0));
     }
+
+    console.log(grid);
 }
